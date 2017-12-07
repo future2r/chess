a270c2b618bdf15dfa99b0a7137ae236e573e34e
Frank Ulbricht <f.ulbricht@qualitype.de>
2017-12-07 10:59:12 +0100

    Added more PGN simulation tests

--- pgn/src/test/java/name/ulbricht/chess/pgn/PGNReadTest.java
+++ pgn/src/test/java/name/ulbricht/chess/pgn/PGNReadTest.java
@@ -5,29 +5,22 @@ import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
-import java.util.List;
 import java.util.stream.Stream;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 final class PGNReadTest {
 
-    private static Stream<Arguments> createReadAllGamesArguments() {
-        return Stream.of(
-                Arguments.of("Kasparov.pgn", 2083),
-                Arguments.of("Wikipedia (de).pgn", 1),
-                Arguments.of("Wikipedia (en).pgn", 1)
-        );
+    private static Stream<Arguments> createReadAllGamesArguments() throws IOException {
+        Path filesPath = Paths.get(System.getProperty("user.dir")).getParent().resolve("files");
+        return Files.find(filesPath, 1, (file, attr) -> file.getFileName().toString().endsWith(".pgn"))
+                .map(file -> Arguments.of(file));
     }
 
     @ParameterizedTest(name = "{index}: {0}")
     @MethodSource("createReadAllGamesArguments")
-    void readAllGames(String fileName, int gameNumber) throws IOException {
-        Path filePath = Paths.get(System.getProperty("user.dir")).getParent().resolve("files").resolve(fileName);
-        List<PGNGame> games = PGN.readGames(filePath);
-
-        assertEquals(gameNumber, games.size());
+    void readAllGames(Path file) throws IOException {
+        PGN.readGames(file);
     }
 }
